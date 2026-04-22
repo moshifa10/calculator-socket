987b3953f381279e4ea8ad99a66b25ad202613a8
Njabulo Moshifa <njmosjhb025@student.wethinkcode.co.za>
2026-04-22 16:26:41 +0200

    added

--- src/main/java/server/CalculatorServer.java
+++ src/main/java/server/CalculatorServer.java
@@ -26,35 +26,45 @@ public class CalculatorServer {
             System.out.println("Client have connected");
 
             // accept input from the client
-            PrintWriter writer = new PrintWriter(clientAccept.getOutputStream());
             BufferedReader readClientInput = new BufferedReader(new InputStreamReader(clientAccept.getInputStream()));
 
-            writer.println("Can I perform an operation for you : (add/sub/div/multi) nums1 num2 <- In this format.");
+//            writer.println("Can I perform an operation for you : (add/sub/div/multi) nums1 num2 <- In this format.");
             String answer = readClientInput.readLine();
 
+            PrintWriter writer = new PrintWriter(clientAccept.getOutputStream(), true);
+
             String[] command = answer.split(" ");
-            Double calcAnswer ;
-            switch (command[0]){
+//            System.out.println(command[0]);
+//            System.out.println(command[1]+ " " + command[1]);
+//            System.out.println(command[0].equals("add"));
+
+            Double calcAnswer;
+            switch (command[0]) {
                 case "add":
                     Addition addition = new Addition();
+                    System.out.println("I am in here");
                     calcAnswer = addition.execute(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
-
-                    writer.println("Answer: " + calcAnswer);
+                    writer.println("Server gives answer: " + calcAnswer);
+                    System.out.println(calcAnswer);
+                    break;
                 case "sub":
                     Subraction subraction = new Subraction();
                     calcAnswer = subraction.execute(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
 
-                    writer.println("Answer: " + calcAnswer);
+                    writer.println("Server gives answer: " + calcAnswer);
+                    break;
                 case "div":
-                    Division division = new  Division();
+                    Division division = new Division();
                     calcAnswer = division.execute(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
 
-                    writer.println("Answer: " + calcAnswer);
+                    writer.println("Server gives answer: " + calcAnswer);
+                    break;
                 case "multi":
                     Multiplication multiplication = new Multiplication();
                     calcAnswer = multiplication.execute(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
 
-                    writer.println("Answer: " + calcAnswer);
+                    writer.println("Server gives answer: " + calcAnswer);
+                    break;
                 default:
                     throw new IllegalArgumentException("Illegal command");
             }
--- src/main/java/server/ClientHandler.java
+++ src/main/java/server/ClientHandler.java
@@ -1,4 +1,27 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ClientHandler {
+
+    public static void main(String[] args){
+        try {
+            Socket client = new Socket("localhost",2003);
+            BufferedReader readFromServer = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.print("Can I perform an operation for you : (add/sub/div/multi) nums1 num2 <- In this format. : ");
+            String command = readFromServer.readLine();
+            PrintWriter writer =  new PrintWriter(client.getOutputStream(), true);
+            writer.println(command);
+            BufferedReader in  = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            System.out.println(in.readLine());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
